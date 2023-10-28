dc4988dcaf6135c6a8253f9ea0a973c7c7d85e19
Rabin Acharya <rabin7acharya@gmail.com>
2023-10-28 15:50:44 +0545

    ham menu

--- scripts/script.js
+++ scripts/script.js
@@ -48,6 +48,67 @@ tl.from("nav", {
 //   });
 // });
 
+// Select the images and container
+const images = document.querySelectorAll(".info-right img");
+const container = document.querySelector(".info-right");
+
+// Set the initial index and duration
+let currentIndex = 0;
+const duration = 4000; // 4 seconds
+
+// Function to animate the image transition
+function animateImageTransition() {
+  gsap.to(images[currentIndex], {
+    opacity: 0,
+    duration: 0.5,
+    onComplete: () => {
+      // Hide the current image
+      images[currentIndex].style.display = "none";
+      // Increment the index or reset it to 0
+      currentIndex = (currentIndex + 1) % images.length;
+      // Show the next image
+      images[currentIndex].style.display = "block";
+      // Animate the opacity of the next image
+      gsap.to(images[currentIndex], {
+        opacity: 1,
+        duration: 1,
+        delay: 0.5, // Delay for a smooth transition
+        onComplete: () => {
+          // Schedule the next transition
+          setTimeout(animateImageTransition, duration);
+        },
+      });
+    },
+  });
+}
+
+// Start the slider by scheduling the first transition
+setTimeout(animateImageTransition, duration);
+
+const hamBtn = document.querySelector(".ham-btn");
+
+let tx = gsap.fromTo(
+  ".ham-menu",
+  {
+    clippath: "circle(0.5% at 100% 0)",
+    display: "none",
+    duration: 0.5,
+  },
+  {
+    display: "flex",
+    clipPath: "circle(76.1% at 51% 43%)",
+    webkitClipPath: "circle(76.1% at 51% 43%)",
+    duration: 0.5,
+    ease: "power2.out",
+    paused: true,
+  }
+);
+
+hamBtn.addEventListener("click", () => {
+  const isHidden = tx.progress() === 0;
+  isHidden ? tx.play() : tx.reverse();
+});
+
 const lenis = new Lenis();
 
 lenis.on("scroll", (e) => {
